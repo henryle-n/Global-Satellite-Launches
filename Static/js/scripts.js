26e5dda4dddce3131cea3fa49c923fc1aec71be2
Henry Le <henryle.b11@gmail.com>
2020-06-08 07:03:13 -0500

    front-end design

--- Static/js/scripts.js
+++ Static/js/scripts.js
@@ -1,8 +1,3 @@
-/*!
-    * Start Bootstrap - Creative v6.0.2 (https://startbootstrap.com/themes/creative)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-creative/blob/master/LICENSE)
-    */
     (function($) {
   "use strict"; // Start of use strict
 
@@ -44,20 +39,8 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-  // Magnific popup calls
-  $('#portfolio').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
+  $('.database-box').magnificPopup({
+    type: 'ajax'
   });
 
 })(jQuery); // End of use strict
