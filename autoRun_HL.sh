3922e04f760240d87c57b34e1db2d8481bc951cb
Henry Le <henryle.b11@gmail.com>
2020-06-12 00:19:20 -0500

    auto push from Henry

--- autoRun_HL.sh
+++ autoRun_HL.sh
@@ -7,24 +7,26 @@ mag=$'\e[1;35m'
 cyn=$'\e[1;36m'
 nc=$'\e[0m'
 
-echo Brought to you by: ${red}Henry Le${nc}
+echo Brought to you by: ${yel}Henry Le${nc}
 echo -e Version: ${mag}0${nc}, Date: ${mag}Jun, 2020${nc}
 echo =============================
 echo
-echo ${red}Activate Environment${nc}. Please wait...
+echo ${cyn}Activate Environment${nc}. Please wait...
 echo ---------------
 source p2env/Scripts/activate
-echo ${red}Done Activating Environment${nc}.
+echo ${yel}Done Activating Environment${nc}.
 echo ---------------
-echo ${red}Starting up Server and Browser${nc}. Please wait...
-python application.py & python -mwebbrowser http://127.0.0.1:5000/
-
+echo ${red}Starting up Server and Browser${nc}. Please wait... 
+python application.py & sleep 2 & python -mwebbrowser http://127.0.0.1:5000/
+echo ---------------
+echo ${grn}SERVER HAS STARTED !!! ${nc}
+echo =============================
+echo 
+echo ${red}Thank you${nc} for using my Scripts - ${cyn}Henry${grn} ${grn}"(06/2020)"${nc}
+echo
+echo =============================
 echo =============================
 echo
-echo ${grn}Server Shut Down...${nc} Sucessfully!
-echo
-echo Press ${yel}Enter${yel} to Exit...
-echo ${grn}Thank you${nc} for using my Scripts - ${grn}Henry${grn} ${grn}(06/2020)${nc}
 echo
 read
 
