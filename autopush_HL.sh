db92161b6fbb4ebd7abfb7443e91e1b0a436f6ca
Henry Le <henryle.b11@gmail.com>
2020-06-11 23:19:51 -0500

    auto push from Henry

--- autopush_HL.sh
+++ autopush_HL.sh
@@ -5,13 +5,13 @@ grn=$'\e[1;32m'
 blu=$'\e[1;34m'
 mag=$'\e[1;35m'
 cyn=$'\e[1;36m'
-white=$'\e[0m'
+nc=$'\e[0m'
 
-echo Brought to you by: ${red} Henry Le$'\e[0m'
-echo -e Version: ${pp}0${nc}, Date: ${pp}Jun, 2020${nc}
+echo Brought to you by: ${red}Henry Le${nc}
+echo -e Version: ${mag}0${nc}, Date: ${mag}Jun, 2020${nc}
 echo =============================
 echo
-echo Begins ${pp}Git Pulling${nc}. Please wait...
+echo Begins ${red}Git Pulling${nc}. Please wait...
 echo ---------------
 git add .
 git commit -am "auto push from Henry"
@@ -19,7 +19,7 @@ git push
 
 echo =============================
 echo
-echo -e "${green}Git Pushed${nc} Sucessfully!"
+echo ${grn}Git Pushed${nc} Sucessfully!
 echo
-echo Press Enter to Exit...
+echo Press ${blu}Enter${blu} to Exit...
 read
