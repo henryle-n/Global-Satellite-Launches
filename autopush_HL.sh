0e6aa03eb73d57b885ad724d43f7fda363d96179
Henry Le <henryle.b11@gmail.com>
2020-06-11 23:09:08 -0500

    auto push from Henry

--- autopush_HL.sh
+++ autopush_HL.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
-echo Brought to you by Henry Le
-echo Version: 0, Date: Jun, 2020
+
+cyan = '\033[0;31m'
+red ='\033[0;31m'
+green ='\033[0;32m'
+pp = '\033[0;35m'
+nc='\033[0m'
+
+echo "Brought to you by ${cyan}Henry Le${nc}"
+echo Version: ${pp}0${nc}, Date: ${pp}Jun, 2020${nc}
 echo =============================
 echo
-echo Begins Git Pulling. Please wait...
+echo Begins ${pp}Git Pulling${nc}. Please wait...
 echo ---------------
 git add .
-git commit -am %1
+git commit -am "auto push from Henry"
 git push
 
 echo =============================
-echo Git Pushed Sucessfully
+echo
+echo ${green}Git Pushed${nc} Sucessfully
 echo
 echo Press Enter...
 read
