b8ff7d74fa1a91d224b2e9be15ced01c2250d260
Henry Le <henryle.b11@gmail.com>
2020-06-15 23:51:46 -0500

    add new version of AWS, clean up storage

--- HenryAWSpkg/SatelliteClass.py
+++ HenryAWSpkg/SatelliteClass.py
@@ -1,3 +1,12 @@
+
+# Author: Henry Le
+# Date: Jun. 15, 2020
+# version 0 - first release
+# if you some how find this file of mine on github, please feel free to take and modify it. I wish you the best of luck on what you want to do.
+# learning coding is not easy as it's about learning a new language!
+ 
+ 
+ 
 # dependencies
 import pandas as pd
 import numpy as np
@@ -6,8 +15,8 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, inspect, join, outerjoin, MetaData, Table
 
+# creating reflective base to query data from sqlite db
 class SatLaunch():
-
     def __init__(self, connect_string):
         self.engine = create_engine(connect_string)
         # self.conn = self.engine.connect()
@@ -23,8 +32,8 @@ class SatLaunch():
         self.SatCount40yr = self.Base.classes['Launch_by_Country_46yr']
         self.Top10LaunchDates = self.Base.classes['Top10_Launch_Dates']
         self.ChoroPlethMap = self.Base.classes['ChoroplethMapData-Henry-Updates']
-        
-        
+    
+    # self check to make sure I have all tables
     def display_db_info(self):
         inspector = inspect(self.engine)
         tables = self.inspector.get_table_names()
@@ -36,7 +45,7 @@ class SatLaunch():
             for column in inspector.get_columns(table):
                 print(f"name: {column['name']}   column type: {column['type']}")
 
-
+    # create meta data to get table desciption
     def meta_info(self):
         inspector = inspect(self.engine)
         tables = self.inspector.get_table_names()
@@ -46,7 +55,6 @@ class SatLaunch():
             ea_metadata = {
                 "Table_Name" : table
             }
-            
             new_dict=new_dict={
             "column_name" : [],
             "column_type" : []
@@ -69,6 +77,7 @@ class SatLaunch():
         session.close()  
         return list(df.Satellite_Names)
     
+
     # get all demo data based on chosen satellite name
     def get_satbio_data(self, sat_name=""):
         session = Session(self.engine)
@@ -94,6 +103,7 @@ class SatLaunch():
         session.close()  
         return df.to_dict(orient="records")
     
+
     # 40 years of satellite launch counts by country 
     def get_40yr_sat_lauch_by_country(self, country_name=""):
         session = Session(self.engine)
@@ -130,7 +140,7 @@ class SatLaunch():
         session.close()  
         return df.to_dict(orient="records")
 
-
+    # country satellite counts
     def get_countryCount_perYear_perCountry(self, country_name=""):
         session = Session(self.engine)
 
@@ -145,13 +155,11 @@ class SatLaunch():
         return df.to_dict(orient="records")
 
 
-
-                      
+# if file is run as main i.e. run from this file, the following commands are executed
 if __name__ == '__main__':
-    info = SatLaunch("sqlite:///Data/UCS_Satellite.db")
+    info = SatLaunch("sqlite:///data/UCS_Satellite.db")
     info.display_db_info()
     info.meta_info()
-
     # print("\nAll Satellite Names:\n", info.get_satellite_names())
     # print("\nSatellite 1HOPSAT-TD:\n", info.get_demographic_data("1HOPSAT-TD (1st-generation High Optical Performance Satellite)"))
     # print("\nAll Satellite-Owned Countries\n", info.get_40yr_sat_lauch_by_country())
@@ -160,8 +168,3 @@ if __name__ == '__main__':
     # print("\nAll Satellite-Owned Countries\n", info.get_launch_date())
     # print("\nTop 10 Launch Dates\n", info.get_top10_launch_dates())
     # print("\nGet Country Name by Year\n", info.get_countryCount_perYear_perCountry())
-
-
-
-
-
--- HenryAWSpkg/application.py
+++ HenryAWSpkg/application.py
@@ -1,52 +1,54 @@
+
+# Author: Henry Le
+# Date: Jun. 15, 2020
+# version 0 - first release
+# if you some how find this file of mine on github, please feel free to take and modify it. I wish you the best of luck on what you want to do.
+# learning coding is not easy as it's about learning a new language!
+
 # dependencies
 from  SatelliteClass import SatLaunch 
 from flask import Flask, jsonify, render_template
 
-#################################################
-# Database Setup
-#################################################
-data = SatLaunch("sqlite:///UCS_Satellite.db")
 
-#################################################
-# Flask Setup
-#################################################
+################### DB Connection #################
+data = SatLaunch("sqlite:///data/UCS_Satellite.db")
+
+################## Flask App set up###############
 app = Flask(__name__)
 
 
-#################################################
-# Flask Routes
-#################################################
-
-# main home page route - this will be static
-# big jumpotron and learn more
+####### custome routes for website and data######
+# main home page route
 @app.route("/")
 def home_page():
     ''' Home Page Access Route'''
     return render_template("index.html")
 
+# to data filterable
 @app.route("/data-filterable")
 def main_page():
     ''' Data Table with filters '''
     return render_template("data-filterable.html")
 
+# for viz page
 @app.route("/visualization")
 def data_viz():
     ''' Run data visualization '''
     return render_template("visualization.html")
 
-# route to static HTML of APIs route
+# route to api list page
 @app.route("/api")
 def api_list():
     """List all available API routes"""
     return render_template("api.html")
 
-# route to static HTML Team personal/ professional info
+# route to Team page
 @app.route("/about")
 def about_us():
     """Team rooster and info"""
     return render_template("about.html")
 
-# route to static HTML Team contact info
+# route to contact info
 @app.route("/contact")
 def contact_us():
     '''Team contact info'''
@@ -67,7 +69,7 @@ def get_all_launch_date():
 def get_metadata():
     return jsonify(data.meta_info())   
 
-# route to demographic data 
+# route to satellite bio data 
 @app.route("/api/satellite-bio")
 def get_all_demoG_data():
     return jsonify(data.get_satbio_data())
@@ -82,7 +84,7 @@ def get_selected_demoG_data(satellite_name):
 def get_all_country_counts():
     return jsonify(data.get_40yr_sat_lauch_by_country())
 
-# route to 46 years satellite launch by chosen country, and counts
+# route to 46 years satellite launch by **chosen** country, and counts
 @app.route("/api/lauch-history-by-country/<country_name>")
 def get_selected_country_counts(country_name):
     return jsonify(data.get_40yr_sat_lauch_by_country(country_name))
@@ -98,11 +100,14 @@ def get_master_recs():
 def get_top10_LaunchDates_recs():
     return jsonify(data.get_top10_launch_dates())
 
+# country count per year
 @app.route("/api/countryCountByYear")
 def get_countryCount_Year_Filterable():
     return jsonify(data.get_countryCount_perYear_perCountry())
 
 
-# exe program
+# if program is run from this file ::
 if __name__ == '__main__':
     app.run(debug=True)
+
+
