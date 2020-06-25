3895304a3757f217fd72c168b9234b10ebf8e6d4
Henry Le <henryle.b11@gmail.com>
2020-06-25 04:40:25 -0500

    add zip file

--- static/js/app-data-table.js
+++ static/js/app-data-table.js
@@ -1,91 +1,169 @@
+// ============== Henry's modifications ================
+
 var inputValObj = [];
 
+// store data offline for exporting upon user request
+var offlineData;
 
-d3.json("/api/master-record").then(function (tableData) {
-  // from data.js in oj's HW11
+function getDataOffline(data) {
+  return offlineData = data;
+}
 
-  // Get a reference to the table body
-  var tbody = d3.select("tbody");
+/*
+- convert retrieved data to csv
+- part of process will be also to replace all "," of thousand seperator
+- if comma is not removed, the csv will not work correctly as the program thought
+- the comma from thousand seperator is the comma delimiter
+*/
+function convertToCSV(arr) {
+  var newArr = [];
+  var tempObj = {};
+  arr.forEach(row => {
+    Object.entries(row).forEach(([k, v]) => {
+      v = v.toString().replace(/,/g, '');
+      k = k;
+      tempObj[k] = v;
+    })
+    newArr.push(tempObj);
+    tempObj = {};
+  })
 
-  // SAT LAUNCH values for each column
-  tableData.slice(0, 10).forEach(function (satlaunch) {
+  const array = [Object.keys(newArr[0])].concat(newArr);
+  return array.map(it => {
+    return Object.values(it).toString()
+  }).join('\n');
+}
+
+// this will trigger the dowload of csv
+function download_csv() {
+  baseName = "Filtered-Data_";
+  var currDate = new Date();
+  var month = currDate.getUTCMonth() + 1; //months from 1-12
+  var day = currDate.getUTCDate();
+  var year = currDate.getUTCFullYear();
+  var newdate = month + "-" + day + "-" + year;
+
+  array = convertToCSV(offlineData);
+  console.log("this is offline DATA\n", offlineData);
+  var hiddenElement = document.createElement('a');
+  // hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(array);
+  hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(array);
+  hiddenElement.target = '_blank';
+  hiddenElement.download = `${baseName}${newdate}.csv`;
+  console.log(hiddenElement);
+  hiddenElement.click();
+}
+
+function resetFilter (){
+  d3.selectAll('input').property("value", "");
+}
+
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+function makeTable (data) {
+  data.forEach(dp => {
     // Append one table row `tr` for each UFO Sighting object
     var row = tbody.append("tr");
-
     // Use `Object.entries` to console.log each UFO Sighting value
-    Object.entries(satlaunch).forEach(function ([key, value]) {
-      //   console.log(key, value);
+    Object.values(dp).forEach(value => {
+
       // Append a cell to the row for each value
       var cell = row.append("td");
       cell.text(value);
     });
   });
+}
 
+
+function runFilter (data) {
+  event.preventDefault();
+  tbody.html("");
+  var filteredData = data;
+  // =============== GET FORM INPUT ======================
+
+  // id array of all input boxes
+  var idObj = {
+    Launch_Date: "#datetime",
+    Country_of_Operator_Owner: "#country",
+    Launch_Site: "#launch_site",
+    Purpose: "#purpose",
+    Orbit_Classes: "#orbitclasses",
+  };
+
+  Object.entries(idObj).forEach(([key, value]) => {
+    // get the user input value from the UI form
+    inpVal = d3.select(value).property("value");
+
+    if (inpVal != "") {
+      inputValObj.push(inpVal);
+      inpVal = inpVal.toLowerCase();
+      console.log("this is inputVAL :: ", inpVal);
+      // push the key with value into object
+      filteredData = filteredData.filter(
+        (record) => record[key].toString().toLowerCase() == inpVal
+      );
+    }
+     else {
+       filteredData = filteredData;
+      };
+  });
+
+  // get new offlineData for CSV
+  getDataOffline(filteredData);
+  makeTable(filteredData);
+
+  // check how many records retrieved
+  var ftrDLen = Object.keys(filteredData).length
+
+  // let user know when data retrieval & table are finished
+  var status = d3.select("#sitRep").text("STATUS:")
+    .append("p").attr("id", "user-notif")
+    .text("Finished Retrieving : " + ftrDLen + " Records")
+    .append("p").attr("id", "usNoteDate")
+    .text(new Date());
+
+  // if filter is not in table's data, tell user to check input
+  if (inputValObj.length != 0 && ftrDLen == 0) {
+    status.append("p").attr("id", "noData")
+      .text("No data found, please check filters and try again...");
+  }
+
+  // if filter is not input, notify user
+  else if (inputValObj.length == 0 && ftrDLen != 0) {
+    status.append("p").attr("id", "noData")
+      .text("WARNING: No filter applied, please input at least one & try again ...");
+  }
+};
+
+// onclick will download the csv
+document.getElementById("csv-btn").onclick = download_csv;
+
+// onclick to clear out all filter
+document.getElementById("reset-btn").onclick = resetFilter;
+
+
+
+
+// ============== Henry's & OJ's modifications ================
+
+d3.json("/api/master-record").then(function (tableData) {
+
+  // get initial offlineData for CSV & initial filtered Data
+  getDataOffline(tableData);
+  
+  // make initial table upon page loading, limit for only 100 records 
+  // to improve webpage loading performance
+  makeTable(tableData.slice(0,100));
+  
   // Select the run filter button
   var button = d3.select("#filter-btn");
+  var inpForm = d3.select("#input-form");
   button.on("click", function () {
-    tbody.html("");
-
-    // =============== GET FORM INPUT ======================
-
-    // id array of all input boxes
-    var idObj = {
-      Launch_Date: "#datetime",
-      Country_of_Operator_Owner: "#country",
-      Launch_Site: "#launch_site",
-      Purpose: "#purpose",
-      Orbit_Classes: "#orbitclasses",
-    };
-
-    filteredData = tableData;
-    Object.entries(idObj).forEach(([key, value]) => {
-      // get the user input value from the UI form
-      inpVal = d3.select(value).property("value");
-      console.log("this is the user input val :: ", inpVal);
-
-      if (inpVal != "") {
-        inputValObj.push(inpVal);
-        inpVal = inpVal.toLowerCase();
-        // push the key with value into object
-        filteredData = filteredData.filter(
-          (record) => record[key].toString().toLowerCase() == inpVal
-        );
-      }
-    });
-
-    filteredData.forEach(function (selections) {
-      //   console.log(selections);
-      // Append one table row `tr` for each UFO Sighting object
-      var row = tbody.append("tr");
-      // Use `Object.entries` to console.log each UFO Sighting value
-      Object.entries(selections).forEach(function ([key, value]) {
-        // console.log(key, value);
-        // Append a cell to the row for each value
-        var cell = row.append("td");
-        cell.text(value);
-      });
-    });
-    // check how many records retrieved
-    var ftrDLen = Object.keys(filteredData).length
-
-    // let user know when data retrieval & table are finished
-    var status = d3.select("#sitRep").text("STATUS:")
-      .append("p").attr("id", "user-notif")
-      .text("Finished Retrieving : " + ftrDLen + " Records")
-      .append("p").attr("id", "usNoteDate")
-      .text(new Date());
-
-    // if filter is not in table's data, tell user to check input
-    if (inputValObj.length != 0 && ftrDLen == 0) {
-      status.append("p").attr("id", "noData")
-        .text("No data found, please check filters and try again...");
-    }
-
-    // if filter is not input, notify user
-    else if (inputValObj.length == 0 && ftrDLen != 0) {
-      status.append("p").attr("id", "noData")
-        .text("WARNING: No filter applied, please input at least one & try again ...");
-    }
-  });
-});
+    runFilter(tableData)});
+  inpForm.on("submit", function () {
+    runFilter(tableData)});
+}).catch(error => {
+  console.log(error);
+})
 
