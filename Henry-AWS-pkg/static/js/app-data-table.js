959fecb9844d4dd6c470519559b5235ee12006a2
Henry Le <henryle.b11@gmail.com>
2020-06-25 04:31:13 -0500

    add error catching

--- Henry-AWS-pkg/static/js/app-data-table.js
+++ Henry-AWS-pkg/static/js/app-data-table.js
@@ -156,66 +156,9 @@ d3.json("/api/master-record").then(function (tableData) {
     runFilter(tableData)});
   inpForm.on("submit", function () {
     runFilter(tableData)});
-    event.preventDefault();
-  // button.on("click", function () {
-  //   tbody.html("");
-  //   var filteredData = tableData;
-  //   // =============== GET FORM INPUT ======================
-
-  //   // id array of all input boxes
-  //   var idObj = {
-  //     Launch_Date: "#datetime",
-  //     Country_of_Operator_Owner: "#country",
-  //     Launch_Site: "#launch_site",
-  //     Purpose: "#purpose",
-  //     Orbit_Classes: "#orbitclasses",
-  //   };
-
-  //   Object.entries(idObj).forEach(([key, value]) => {
-  //     // get the user input value from the UI form
-  //     inpVal = d3.select(value).property("value");
-
-  //     if (inpVal != "") {
-  //       inputValObj.push(inpVal);
-  //       inpVal = inpVal.toLowerCase();
-  //       console.log("this is inputVAL :: ", inpVal);
-  //       // push the key with value into object
-  //       filteredData = filteredData.filter(
-  //         (record) => record[key].toString().toLowerCase() == inpVal
-  //       );
-  //     }
-  //      else {
-  //        filteredData = filteredData;
-  //       };
-  //   });
-
-  //   // get new offlineData for CSV
-  //   getDataOffline(filteredData);
-  //   makeTable(filteredData);
-
-  //   // check how many records retrieved
-  //   var ftrDLen = Object.keys(filteredData).length
-
-  //   // let user know when data retrieval & table are finished
-  //   var status = d3.select("#sitRep").text("STATUS:")
-  //     .append("p").attr("id", "user-notif")
-  //     .text("Finished Retrieving : " + ftrDLen + " Records")
-  //     .append("p").attr("id", "usNoteDate")
-  //     .text(new Date());
-
-  //   // if filter is not in table's data, tell user to check input
-  //   if (inputValObj.length != 0 && ftrDLen == 0) {
-  //     status.append("p").attr("id", "noData")
-  //       .text("No data found, please check filters and try again...");
-  //   }
-
-  //   // if filter is not input, notify user
-  //   else if (inputValObj.length == 0 && ftrDLen != 0) {
-  //     status.append("p").attr("id", "noData")
-  //       .text("WARNING: No filter applied, please input at least one & try again ...");
-  //   }
-  // });
-});
+}).catch(error => {
+  console.log(error);
+})
 
 // onclick will download the csv
 document.getElementById("csv-btn").onclick = download_csv;
